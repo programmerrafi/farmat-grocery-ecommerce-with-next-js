b886c95a5347af2f82455704dbdc771b95871864
Md Rafi <mdrafiprogrammer@gmail.com>
2022-09-08 15:28:19 +0600

    farmat-grocery design is running now

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import FeaturedBrands from "../app/components/Home/FeaturedBrands";
+import HomeCategory from "../app/components/Home/HomeCategory";
+import HomeHero from "../app/components/Home/HomeHero";
+
+export default function Home() {
+  return (
+    <>
+      <HomeHero />
+      <HomeCategory />
+      <FeaturedBrands />
+    </>
+  )
+}
