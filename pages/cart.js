c6a2f994762fd9cdd8a46c0037509de8acde45e7
Md Rafi <mdrafiprogrammer@gmail.com>
2022-10-01 20:42:37 +0600

    e-commrece app functionality is runnning

--- pages/cart.js
+++ pages/cart.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import CartActions from "../app/components/cart/CartActions"
+import CartTable from "../app/components/cart/CartTable"
+import Breadcrumb from "../app/components/shared/Breadcrumb"
 
 const Cart = () => {
     return (
-        <div>Cart</div>
+        <main>
+            <div className="bg-[#F5F5F5] p-5">
+                <div className="mx-auto max-w-5xl">
+                    <Breadcrumb />
+                </div>
+            </div>
+            <div className="p-5">
+                <div className="mx-auto max-w-5xl mt-10">
+                    <h1 className="text-4xl text-center text-title font-bold mb-10">Cart</h1>
+                    <CartTable />
+                    <CartActions />
+                </div>
+            </div>
+        </main>
     )
 }
 
